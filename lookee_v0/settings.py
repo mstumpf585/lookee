18c65a15377b53c16e4e1251066902b305c2575c
mike <pro585g@gmail.com>
2018-03-10 10:20:58 -0800

    DB working

--- lookee_v0/settings.py
+++ lookee_v0/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'lookee_v0.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'testcomicdb',
+	'USER':'testcomicdbbasicuser',
+	'PASSWORD': 'Mike601',
+	'HOST': 'localhost',
+	'PORT': '',
     }
 }
 
